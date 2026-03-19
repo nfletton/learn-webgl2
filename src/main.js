334340e676ab409ec5f60b9fd56d9af0fc66d7c8
Nigel Fletton <nigel@thebluehut.com>
2026-03-19 14:14:58 -0300

    Add gl-matrix library

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import './style.css'
+import {vec2} from "gl-matrix";
 // import javascriptLogo from './javascript.svg'
 // import viteLogo from '/vite.svg'
 // import { setupCounter } from './counter.js'
@@ -9,3 +10,9 @@ document.querySelector('#app').innerHTML = `
 
 const canvas = document.getElementById('canvas');
 const gl = canvas.getContext('webgl2');
+
+var v1 = vec2.fromValues(4, 1);
+var v2 = vec2.fromValues(2, 2);
+var result = vec2.create()
+vec2.add(result, v1, v2);
+console.log(result.toString())
